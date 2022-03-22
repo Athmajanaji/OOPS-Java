b2f26df2583797bb252922bebbce25509430ab0d
Athmajan A S <73539585+Athmajanaji@users.noreply.github.com>
2022-03-22 14:22:58 +0530

    Add files via upload

---
+++ Area/Area.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Area{
+public static void main(String a[]){
+ Scanner obj = new Scanner(System.in);
+ System.out.println("Enter the breadth");
+ int b=obj.nextInt();
+ System.out.println("Enter the length");
+ int l=obj.nextInt();
+ System.out.print(l*b);
+}
+}
